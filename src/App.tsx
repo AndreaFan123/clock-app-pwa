f644e2e7dea10bf5b0bff37df59e933e135de622
andrea.fan <84858081+AndreaFan123@users.noreply.github.com>
2024-02-21 08:37:44 +0800

    Add Jest and SWR dependencies, update package.json and pnpm-lock.yaml, and implement API call in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import useSWR from "swr";
+
 function App() {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "http://worldtimeapi.org/api/timezone/Asia/Taipei",
+    fetcher
+  );
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loading...";
+
+  console.log(data);
   return (
     <div>
       <h1>Hello World</h1>
